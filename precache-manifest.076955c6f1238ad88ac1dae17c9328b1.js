63783b32772c9b3bedf84c9542f73a27e67e6a05
programerr01 <nitesh6pandey6@gmail.com>
2020-10-25 21:01:06 +0530

    Updates

---
+++ precache-manifest.076955c6f1238ad88ac1dae17c9328b1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cff06f3bd8648882bfe4a628d532912",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "e7bf0f3f89c40e257c9c",
+    "url": "/shopping-cart/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5041eee1d9291d21a75a",
+    "url": "/shopping-cart/static/css/main.1e0214d6.chunk.css"
+  },
+  {
+    "revision": "e7bf0f3f89c40e257c9c",
+    "url": "/shopping-cart/static/js/2.03c896eb.chunk.js"
+  },
+  {
+    "revision": "3448068023a6edfd281e51df8812361f",
+    "url": "/shopping-cart/static/js/2.03c896eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5041eee1d9291d21a75a",
+    "url": "/shopping-cart/static/js/main.337d6340.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "01ae4f6e3a6087465003a28054132275",
+    "url": "/shopping-cart/static/media/adidas1.01ae4f6e.jpg"
+  },
+  {
+    "revision": "67c79330e9b84647eb3ba4745f6e2caf",
+    "url": "/shopping-cart/static/media/adidas_f.67c79330.png"
+  },
+  {
+    "revision": "a3821d1570b78cbb2b4223d3eff3c410",
+    "url": "/shopping-cart/static/media/louis.a3821d15.jpg"
+  },
+  {
+    "revision": "ebdaac4b017d5eca196a84205c98f235",
+    "url": "/shopping-cart/static/media/nike1.ebdaac4b.jpg"
+  },
+  {
+    "revision": "ac189e7c8c0282834cc3e2ca7e6d3e1f",
+    "url": "/shopping-cart/static/media/redchieft.ac189e7c.jpg"
+  },
+  {
+    "revision": "562bba166c4a8d7476eab5c75b80a2bc",
+    "url": "/shopping-cart/static/media/reebok1.562bba16.jpg"
+  },
+  {
+    "revision": "1845ae505a860f111b933e20a6cab3fc",
+    "url": "/shopping-cart/static/media/woodland.1845ae50.jpg"
+  }
+]);
